f2340c9446bdb29013499dc8899ac3f2fced2211
Brian Schlining <bschlining@gmail.com>
2026-03-05 15:00:16 -0800

    new way to grab application version

--- src/main/java/org/mbari/charybdis/AppConfig.java
+++ src/main/java/org/mbari/charybdis/AppConfig.java
@@ -1,10 +1,14 @@
 package org.mbari.charybdis;
 
+import org.eclipse.microprofile.config.ConfigProvider;
+
 public class AppConfig {
 
     public static final String NAME = "charybdis";
 
-    public static final String VERSION = AppConfig.class.getPackage().getImplementationVersion();
+    public static final String VERSION = ConfigProvider.getConfig()
+            .getOptionalValue("quarkus.application.version", String.class)
+            .orElse("unknown");
 
     public static final String DESCRIPTION = "Annotation and Media Services for MBARI";
 
