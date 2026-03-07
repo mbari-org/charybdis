58ee02be401a0323f6398f224ac8a7e3df33ac0d
Brian Schlining <bschlining@gmail.com>
2026-03-07 08:53:23 -0800

    Fixes to snake vs camel case

--- src/main/java/org/mbari/charybdis/etc/jackson/CharybdisModuleCustomizer.java
+++ src/main/java/org/mbari/charybdis/etc/jackson/CharybdisModuleCustomizer.java
@@ -2,9 +2,11 @@ package org.mbari.charybdis.etc.jackson;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import io.quarkus.jackson.ObjectMapperCustomizer;
+import jakarta.annotation.Priority;
 import jakarta.inject.Singleton;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.mbari.vars.annosaurus.sdk.r1.models.Annotation;
@@ -12,9 +14,10 @@ import org.mbari.vars.vampiresquid.sdk.r1.models.Media;
 import org.mbari.vcr4j.time.Timecode;
 
 @Singleton
+@Priority(1)
 public class CharybdisModuleCustomizer implements ObjectMapperCustomizer {
 
-    @ConfigProperty(name = "quarkus.jackson.property-naming-strategy")
+    @ConfigProperty(name = "charybdis.jackson.property-naming-strategy", defaultValue = "LOWER_CAMEL_CASE")
     String propertyNamingStrategy;
 
     public void customize(ObjectMapper mapper) {
@@ -27,6 +30,7 @@ public class CharybdisModuleCustomizer implements ObjectMapperCustomizer {
         // after the face and explicity set the names. Here we just chane the mixin based on the
         // strategy.
         if (propertyNamingStrategy.equals("SNAKE_CASE")) {
+            mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
             mapper.addMixIn(Annotation.class, AnnotationMixinSC.class);
             mapper.addMixIn(Media.class, MediaMixinSC.class);
         }
