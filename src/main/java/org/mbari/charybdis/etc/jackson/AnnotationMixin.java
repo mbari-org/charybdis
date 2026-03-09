9c0bb4a58df8c84fd26b049ccf2510c539cfe384
Brian Schlining <bschlining@gmail.com>
2026-03-09 08:26:51 -0700

    removed redendant image data from annotation JSON

--- src/main/java/org/mbari/charybdis/etc/jackson/AnnotationMixin.java
+++ src/main/java/org/mbari/charybdis/etc/jackson/AnnotationMixin.java
@@ -3,14 +3,19 @@ package org.mbari.charybdis.etc.jackson;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.mbari.vars.annosaurus.sdk.r1.models.ImageReference;
 import org.mbari.vcr4j.VideoIndex;
 
 import java.time.Duration;
+import java.util.List;
 
 public abstract class AnnotationMixin {
 
     @JsonIgnore
     abstract VideoIndex getVideoIndex();
+
+    @JsonIgnore
+    abstract List<ImageReference> getImages();
 
     @JsonSerialize(using = DurationSerializer.class)
     @JsonProperty("durationMillis")
